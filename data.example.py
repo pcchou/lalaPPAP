86d6699af0ca299f7abfe1cb54956069ae7d2f1c
Philip Ping-yu Chou <pcchou@pcchou.me>
2016-12-26 01:38:31 +0800

    Add data.example.py

---
+++ data.example.py
@@ -0,0 +1,4 @@
+MONITOR = [] # uid for monitoring, just for test :P
+
+UA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0"
+COOKIE = {'c_user': "== FILL IN YOURSELF ==", 'xs': "== FILL IN YOURSELF =="}
